66b7a41c5bd99e142bb3b63d9a64f574d0f4668d
Tony Zhang <tozhang665@gmail.com>
2021-07-28 23:21:02 -0700

    added rspec notes

---
+++ W4D4/Rspec/spec/dessert_spec.rb
@@ -0,0 +1,50 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef") }
+  let(:dessert){Dessert.new("sweets",5,"Steven")}
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(dessert.type).to eq("sweets")
+    end
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(5)
+    end
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to eq([])
+    end
+    # it "raises an argument error when given a non-integer quantity" do
+    #   expect(Dessert.new("","five","guy")).to raise_error("Not a Integer")
+    # end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do 
+      
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array"
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity"
+
+    it "raises an error if the amount is greater than the quantity"
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name"
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in"
+  end
+end
